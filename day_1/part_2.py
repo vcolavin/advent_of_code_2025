a3e586910bbda9f9987d4b94bf97f99371d3046d
Vincent Colavin <vcolavin@gmail.com>
2025-12-02 23:21:54 -0800

    trying to solve part 2...

--- day_1/part_2.py
+++ day_1/part_2.py
@@ -8,46 +8,46 @@ csv_file = open('test_input.csv')
 rows = csv.reader(csv_file)
 
 def count_matches(n):
-  return n // 100
+  return abs(n) // 100
 
-for row in rows:
+def mod_diff(start, end):
+  return abs(count_matches(end) - count_matches(start))
+
+end = current_position
+for (index, row) in enumerate(rows):
   val = row[0]
   my_list = list(val)
   direction = my_list[0]
   number = int("".join(my_list[1:]))
 
-  initial_position = current_position
+  start = end
 
   if direction[0] == "R":
-    current_position += number
+    end += number
   else:
-    current_position -= number
+    end -= number
 
-  count += abs(count_matches(current_position) - count_matches(initial_position))
+  diff = mod_diff(start, end)
 
-  if current_position % 100 == 0:
-    count += 1
+
+  print(f"{index + 1}.", end="")
+  print("start", start, "end", end)
+
+  if (end == 0):
+    print("adding one for ending at zero")
+    diff += 1
+  elif ((abs(start) != start) != (abs(end) != end)):
+    print("adding one for traversing zero")
+    diff += 1
+
+  count += diff
+
+
+  print("diff", diff, "count", count)
 
 
 print(count)
 
 
-# count how many numbers between N and M modulo 100 == 0
-# is it func(M) - func(N)?
-
-# n = 569
-# func(n) = 5
-# m = 892
-# func(m) = 8
-
-# func(m) - func(n) = 3
-
-# start: -10
-# end: 10
-# distance: 20
-# end - start, 10 - (-10)
-
-# start: 10
-# end: -10
-# distance: 20
-# end - start, -10 - 10
+# how many numbers between (n and m] are divisible by 100?
+#
